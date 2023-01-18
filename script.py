6c595a163356885b7398d172ba6b7f32a4a6b7a0
web-Nuo <mlocht@outlook.com>
2023-01-18 21:58:24 +0100

    add barebones fetch drivetime function

--- script.py
+++ script.py
@@ -3,10 +3,16 @@ import geopandas as gpd
 import pandas as pd
 import folium
 import uuid
+from datetime import date
+import json
+import openrouteservice
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
+
 
 def csv_to_gdf(filepath):
     df = pd.read_csv(filepath, nrows=0)
-
 
     if not set(['id','latitude','longitude']).issubset(df.columns):
         s = ','.join(map(str, list(df.columns)))
@@ -22,8 +28,31 @@ def csv_to_gdf(filepath):
 
     return gdf
 
+
+def fetch_drivetime(poi, time_range):
+    # authentication OpenRouteService
+    secret = os.getenv("ORS_SECRET")
+    ors = openrouteservice.Client(key=secret)
+
+    isochrone = ors.isochrones(locations=[[poi['longitude'], poi['latitude']]], range=[time_range])
+
+    isochrone['features'][0]['properties']['id'] = poi['id']
+
+    with open(f"./geojson/{poi['id']}_{date.today()}.geojson", 'w') as output_file:
+        json.dump(isochrone, output_file, ensure_ascii=False, indent=4)
+
     
+
+#######    
 
 gdf = csv_to_gdf("./dummy/p.csv")
 
-print(gdf.head())
+# print(gdf.head())
+
+# poi = {
+#     'longitude': 4.4158472,
+#     'latitude': 51.2452363,
+#     'id': 'Antwerp-Kinepolis'
+# }
+
+# fetch_drivetime(poi, 900)
